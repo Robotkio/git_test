768b725d1cff69960f9e1d3790b057d01373dd10
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-26 14:39:26 -0700

    add rock-paper-scissors.js and import it into index page

---
+++ rock-paper-scissors.js
@@ -0,0 +1,11 @@
+// start game
+// get user input rock, paper, scissors, quit
+// if quit, end
+// if not quit, randomize computer rps
+// compare user and bot
+// update scores
+// goto 2
+
+function playRockPaperScissors() {
+    
+}
